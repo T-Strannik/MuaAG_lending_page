160e1ef0192431a52b5ffea1491b0fb67435b046
Tygran Gorelov <31432827+T-Strannik@users.noreply.github.com>
2017-08-29 22:22:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mua-AG</title>
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Comfortaa:300,400,700&amp;subset=cyrillic" rel="stylesheet"> 
+
+	<link href="https://fonts.googleapis.com/css?family=Pangolin&amp;subset=cyrillic" rel="stylesheet"> 
+
+	<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/animate.css">
+
+	<link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+	<link rel="stylesheet" href="css/column.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+	<div class="container">
+
+		<div class="header">
+			<div class="row">
+				<div class="left-column ">
+					<img src="images/logo.png" alt="">
+					<div id="mak">
+						Макияж:<br>
+						<a href="#" data-src="#wedding" class="order"><p>- свадебный</p></a>
+						<a href="#" data-src="#events" class="order"><p>- вечерний</p></a>
+						<a href="#" data-src="#photosession" class="order"><p>- на фотосессию</p></a>
+						<a href="#" data-src="#working" class="order"><p>- деловой</p></a>
+					</div>
+				</div>
+				<div class="right-column">
+					<img src="images/head4.jpg" alt="">
+				</div>
+			</div>
+		</div>
+			
+		<div class="menu">
+			<div>
+				<a href="#master">
+					<div> <!-- class="animated infinite bounceInLeft" -->
+						<i class="fa fa-user-circle-o" aria-hidden="true"></i><br>О мастере
+					</div>
+				</a>	
+			</div>
+
+			<div>
+				<a href="#gallery">
+					<div> <!-- class="animated infinite bounceInUp" -->
+						<i class="fa fa-picture-o" aria-hidden="true"></i><br>Галлерея
+					</div>	
+				</a>
+			</div>
+
+			<div>
+				<a href="#contacts"">
+					<div> <!-- class="animated infinite bounceInRight" -->
+						<i class="fa fa-phone" aria-hidden="true"></i><br>Контакты
+					</div>
+				</a>	
+			</div>
+		</div>
+
+		<div id="master">
+			<div>
+				<img src="images/avatar.jpg" alt="">
+			</div>
+			<div>
+				Здравствуйте, милые девушки! <br>
+				Рада приветствовать Вас на моем сайте! <br>
+				Каждая из нас мечтает быть волшебной феей, вдохновлять и очаровывать своей красотой. <br>
+				Хватит мечтать! Нужно делать! <br>
+				А я буду Вам помогать.<br><br><br>
+				Немного о себе: <br>
+				В апреле 2016 года окончила базовый курс "Салонный мастер" в школе макияжа Анны Стадник. <br>
+				Апрель, 2016: повышение квалификации в рамках мастер-класса в школе макияжа Анны Стадник "Свадебный и вечерний макияж: актуальные техники, гелевые текстуры, стробинг".<br>
+				Ноябрь, 2016: участие в Всеукраинском фестивале мастеров бьюти-индустрии "Зеркало моды 2016", г. Запорожье.<br>
+			</div>
+		</div>
+
+		
+		
+
+		
+		<div id="gallery" class=" row">
+
+			<div class="left-col">
+
+				<a href="#" data-src="#wedding" class="order">
+					<div class="gallery-menu">
+						<h3>Свадебный макияж</h3>
+						<img src="images/slides/wedding/w2.jpg" alt="">
+					</div>
+				</a>
+				
+				<a href="#" data-src="#photosession" class="order">
+					<div class="gallery-menu">
+						<h3>На фотосессию</h3>
+						<img src="images/slides/photos/p1.jpg" alt="">
+					</div>
+				</a>
+
+			</div>
+
+			<div class="right-col">
+				<a href="#" data-src="#events" class="order">
+					<div class="gallery-menu">
+						<h3>Вечерний макияж</h3>
+						<img src="images/slides/events/e3.jpg" alt="">
+					</div>
+				</a>
+
+				<a href="#" data-src="#working" class="order">
+					<div class="gallery-menu">
+						<h3>Деловой макияж</h3>
+						<img src="images/slides/working/wor4.jpg" alt="">
+					</div>
+				</a>
+			</div>
+			
+			<div id="wedding">
+				<div class="fotorama" data-nav="thumbs">
+					<a href="images/slides/wedding/w1.jpg"><img src="images/slides/wedding/w1.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w2.jpg"><img src="images/slides/wedding/w2.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w3.jpg"><img src="images/slides/wedding/w3.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w4.jpg"><img src="images/slides/wedding/w4.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w5.jpg"><img src="images/slides/wedding/w5.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w6.jpg"><img src="images/slides/wedding/w6.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w7.jpg"><img src="images/slides/wedding/w7.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w8.jpg"><img src="images/slides/wedding/w8.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/wedding/w9.jpg"><img src="images/slides/wedding/w9.jpg" alt="" width="144" height="96" ></a>
+				</div>
+			</div>
+
+			<div id="photosession">
+				<div class="fotorama" data-nav="thumbs">
+					<a href="images/slides/photos/p1.jpg"><img src="images/slides/photos/p1.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/photos/p2.jpg"><img src="images/slides/photos/p2.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/photos/p3.jpg"><img src="images/slides/photos/p3.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/photos/p4.jpg"><img src="images/slides/photos/p4.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/photos/p5.jpg"><img src="images/slides/photos/p5.jpg" alt="" width="144" height="96" ></a>
+				</div>
+			</div>
+
+			<div id="events">
+				<div class="fotorama" data-nav="thumbs">
+					<a href="images/slides/events/e1.jpg"><img src="images/slides/events/e1.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/events/e2.jpg"><img src="images/slides/events/e2.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/events/e3.jpg"><img src="images/slides/events/e3.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/events/e4.jpg"><img src="images/slides/events/e4.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/events/e5.jpg"><img src="images/slides/events/e5.jpg" alt="" width="144" height="96" ></a>
+				</div>
+			</div>
+
+			<div id="working">
+				<div class="fotorama" data-nav="thumbs">
+					<a href="images/slides/working/wor1.jpg"><img src="images/slides/working/wor1.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/working/wor2.jpg"><img src="images/slides/working/wor2.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/working/wor3.jpg"><img src="images/slides/working/wor3.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/working/wor4.jpg"><img src="images/slides/working/wor4.jpg" alt="" width="144" height="96" ></a>
+					<a href="images/slides/working/wor5.jpg"><img src="images/slides/working/wor5.jpg" alt="" width="144" height="96" ></a>
+				</div>
+			</div>
+		
+
+		</div>
+
+
+		<div id="contacts">
+			<div id="frame">
+				<h1>Свежее видео:</h1>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/65JHUnRCYOY" frameborder="0" allowfullscreen></iframe>
+			</div>
+
+			<div class="col-6">
+				<h1>Свяжитесь со мной:</h1>
+				<h3>Ангелина Горелова</h3>
+				<p>Професиональный визажист, свадебный визажист</p>
+				<div>
+					<h3>Мои контакты:</h3>
+					<p><a href="tel: +380688815274">+38(068)881-52-74</a></p>
+					<p><a href="tel: +380632085455">+38(063)208-55-55</a></p>
+				</div>
+				<h3>Я в соцсетях:</h3>
+				<p><a href="https://www.instagram.com/perfectschoolmakeup/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				<a href="https://vk.com/mua_ag"><i class="fa fa-vk" aria-hidden="true"></i></a>
+				<a href="https://www.youtube.com/channel/UCygjeEYj2Mq96Kwsr8gCIZw"><i class="fa fa-youtube-square" aria-hidden="true"></i></a></p>
+			</div>
+		
+			<div class="col-6">
+				<h1>Обратная связь:</h1>
+				<form action="" class="order-form">
+					<i class="fa fa-fw fa-user" aria-hidden="true"></i>
+					<input type="text" size="20" name="user" required placeholder="Ваш имя">
+					<br>
+					<i class="fa fa-fw fa-phone" aria-hidden="true">
+					</i><input type="text" size="20" name="phone" required placeholder="+38(___) ___-__-__">
+					<br>
+					<i class="fa fa-envelope-o" aria-hidden="true"></i>
+					</i><input type="text" size="20" name="email" required placeholder="Введите Ваш @-mail">
+					<br><input type="submit" value="Отправить">
+				</form>
+			</div>
+		
+		</div>
+
+		<a href="#" id="to_top">Вверх ↑</a>
+
+	</div>
+
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> 
+
+	<script src="js/jquery.fancybox.min.js"></script>
+
+	<script src="js/script.js"></script>	
+</body>
+</html>
